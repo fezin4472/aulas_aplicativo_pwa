eb8fa7dad99d32dc97871fadc5627b268d99133c
FERNANDO FRANÇA FIGUEIREDO LONDERO <fernando_londero@estudante.sesisenai.org.br>
2026-09-09 22:11:00 -0300

    estilo Header

--- src/komponenten/Header/Header.jsx
+++ src/komponenten/Header/Header.jsx
@@ -1,5 +1,11 @@
+import './Header.css'
+
 function Header() {
-    return <header>musetti willn us open trust</header>
+    return <header className='Header_root'>
+        <h4>logo</h4>
+        <h4>idk</h4>
+        <h4>user thing</h4>
+    </header>
 }
 
 export default Header
